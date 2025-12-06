175f4c093afd873b204aa3917b968bc92f529744
MD Fahad Molla <ahfahad47sss@gmail.com>
2025-12-06 21:45:23 +0600

    axois and secure axios added

--- src/routes/AdminRoute.jsx
+++ src/routes/AdminRoute.jsx
@@ -1,17 +1,18 @@
 import Loading from "../components/Loading";
 import useAuth from "../hooks/useAuth";
+import useRole from "../hooks/useRole";
 
 const AdminRoute = ({ children }) => {
   const { loading } = useAuth();
-  //   const { role, roleLoading } = useRole();
+  const { role, roleLoading } = useRole();
 
-  //   if (loading || roleLoading) {
-  //     return <Loading></Loading>;
-  //   }
+  if (loading || roleLoading) {
+    return <Loading></Loading>;
+  }
 
-  //   if (role !== "admin") {
-  //     return <Forbidden></Forbidden>;
-  //   }
+  if (role !== "admin") {
+    return <Forbidden></Forbidden>;
+  }
 
   return children;
 };
--- src/routes/DecoratorRoute.jsx
+++ src/routes/DecoratorRoute.jsx
@@ -1,17 +1,18 @@
 import Loading from "../components/Loading";
 import useAuth from "../hooks/useAuth";
+import useRole from "../hooks/useRole";
 
 const DecoratorRoute = ({ children }) => {
   const { loading, user } = useAuth();
-  //   const { role, roleLoading } = useRole();
+  const { role, roleLoading } = useRole();
 
-  //   if (loading || !user || roleLoading) {
-  //     return <Loading></Loading>;
-  //   }
+  if (loading || !user || roleLoading) {
+    return <Loading></Loading>;
+  }
 
-  //   if (role !== "decorator") {
-  //     return <Forbidden></Forbidden>;
-  //   }
+  if (role !== "decorator") {
+    return <Forbidden></Forbidden>;
+  }
 
   return children;
 };
